48b2ff294312f653bfd8233746869ee741b5bb7d
hellothrive <hello@thrive.be>
2020-03-14 18:48:14 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// let i = 7;
-// let untill = 100;
+// 1. plaats beide waardes: startwaarde en eindwaarde in een variabele (7 en 100)
 
-let i = parseInt(prompt('Startwaarde'));
-let untill = parseInt(prompt('Eindwaarde'));
-let valueOne = parseInt(prompt('Eerste waarde deelbaar'));
-let valueTwo = parseInt(prompt('Tweede waarde deelbaar'));
+//2. Maak een loop die loopt van startwaarde 7, tot eindwaarde 100. 
+//M.A.W. tot zolang de startwaarde kleiner is dan de eindwaarde
+    //3. Ga binnen de loop na als de  startwaarde op dat moment deelbaar is door 3 EN als die deelbaar is door 5
+    //TIP: gerbuik restwaarde %
 
-for(i; i<untill; i++){
-    if(i%valueOne === 0 && i%valueTwo === 0){
-        document.write('bitterbal <br>');
-    }else if(i % valueOne === 0){
-        document.write('bitter <br>');
-    }else if(i%valueTwo === 0){
-        document.write('bal <br>');
-    }else{
-        document.write(i + '<br>');
-    }
-}
+
+
+// Fancy
+// 1. Vraag de startwaarde, eindwaarde, en twee getallen op via prompt
+// 2. Maak een loop van de startwaarde tot de eindwaarde
+        //Controleer indien de huidige startwaarde deelbaar is door getal 1 EN 2
+        // Schrijf bitterbal
+        //Controleer indien de huidige startwaarde deelbaar is door getal 1
+        //Schrijf bitter
+        //Controleer indien de huidige startwaarde deelbaar is door getal 2
+         //Schrijf bal
+         //Anders schrijf je het getal
