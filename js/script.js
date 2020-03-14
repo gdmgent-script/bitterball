3ceb771dcb3b358f220ae3e3a3ee2cc00a5780fd
hellothrive <hello@thrive.be>
2020-03-14 18:38:44 +0100

    wip

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// let i = 7;
+// let untill = 100;
+
+let i = parseInt(prompt('Startwaarde'));
+let untill = parseInt(prompt('Eindwaarde'));
+let valueOne = parseInt(prompt('Eerste waarde deelbaar'));
+let valueTwo = parseInt(prompt('Tweede waarde deelbaar'));
+
+for(i; i<untill; i++){
+    if(i%valueOne === 0 && i%valueTwo === 0){
+        document.write('bitterbal <br>');
+    }else if(i % valueOne === 0){
+        document.write('bitter <br>');
+    }else if(i%valueTwo === 0){
+        document.write('bal <br>');
+    }else{
+        document.write(i + '<br>');
+    }
+}
